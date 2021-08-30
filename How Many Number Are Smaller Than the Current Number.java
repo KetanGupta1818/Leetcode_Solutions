19da7a21bf2a673b28d8d345ba8e485f1d9759c7
KetanGupta1818 <84444599+KetanGupta1818@users.noreply.github.com>
2021-08-30 10:00:38 +0530

    Update How Many Number Are Smaller Than the Current Number.java
    Brute Force: 
    Runtime: 22 ms, faster than 5.70% of Java online submissions for How Many Numbers Are Smaller Than the Current Number.
    Memory Usage: 41.4 MB, less than 7.63% of Java online submissions for How Many Numbers Are Smaller Than the Current Number.
    
    Counting Sort:
    Runtime: 2 ms, faster than 81.29% of Java online submissions for How Many Numbers Are Smaller Than the Current Number.
    Memory Usage: 41.6 MB, less than 5.42% of Java online submissions for How Many Numbers Are Smaller Than the Current Number.

--- How Many Number Are Smaller Than the Current Number.java
+++ How Many Number Are Smaller Than the Current Number.java
@@ -14,3 +14,22 @@ class Solution {
         return arr;
     }
 }
+//Counting Sort: O(n) Time Complexity
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int max = 0,i;
+        for(i=0;i<nums.length;i++){
+            if(nums[i]>max) max = nums[i];
+        }
+        int B[] = new int[nums.length];
+        int C[] = new int[max+1];
+        for(i=0;i<=max;i++) C[i] = 0;
+        for(i=0;i<nums.length;i++) C[nums[i]] = C[nums[i]] + 1;
+        for(i=1;i<=max;i++) C[i] = C[i] + C[i-1];
+        for(i=0;i<nums.length;i++){
+            if(nums[i]==0) B[i]=0;
+            else B[i] = C[nums[i]-1];
+        }
+        return B;
+    }
+}
