7182887a63630b44066c9120ec728f0d753792a3
KetanGupta1818 <84444599+KetanGupta1818@users.noreply.github.com>
2021-09-02 13:32:04 +0530

    Create Swap Node in Paris.c
    With three pointers and no recursion.
    Runtime: 5 ms, faster than 6.18% of C online submissions for Swap Nodes in Pairs.
    Memory Usage: 5.9 MB, less than 65.11% of C online submissions for Swap Nodes in Pairs.

---
+++ Swap Node in Paris.c
@@ -0,0 +1,18 @@
+struct ListNode* swapPairs(struct ListNode* head){
+    if(head==NULL || head->next==NULL) return head;
+    struct ListNode* temp1 = head;
+    struct ListNode* back = head;
+    struct ListNode* temp2 = head->next;
+    head = temp2;
+    while(temp1!=NULL && temp1->next!=NULL){
+        back->next = temp2;
+        temp1->next = temp2->next;
+        temp2->next = temp1;
+        back = temp1;
+        temp1 = temp1->next;
+        if(temp1!=NULL) temp2 = temp1->next;
+    }
+    return head;
+}
+//----------------------------------------------------------------------
+//With Recursion
