f2c2360458d0fe37801716edc1ded25aea50e740
KetanGupta1818 <84444599+KetanGupta1818@users.noreply.github.com>
2021-09-16 13:51:10 +0530

    Create Delete Characters to Make Fancy String.java

---
+++ Delete Characters to Make Fancy String.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String makeFancyString(String s) {
+		int n = s.length();
+		if(n==0 || n==1 || n==2) return s;
+		StringBuffer str = new StringBuffer(s);
+		int count = 0;
+		for(int i=0;i<str.length()-1;i++) {
+			char c1 = str.charAt(i);
+			char c2 = str.charAt(i+1);
+			if(c1 == c2) {
+				count++;
+				if(count >= 2) {
+					str.deleteCharAt(i+1);
+					i--;
+				} 
+			}
+			else count = 0;
+			
+		}
+		return str.toString(); 
+    }
+}
+//Runtime: 2254 ms, faster than 5.01% of Java online submissions for Delete Characters to Make Fancy String.
+//Memory Usage: 83.6 MB, less than 47.47% of Java online submissions for Delete Characters to Make Fancy String.
