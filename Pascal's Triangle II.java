b1a268270a0f7ddd7c1f8a1b0ff8fb0dbf5526e5
KetanGupta1818 <84444599+KetanGupta1818@users.noreply.github.com>
2021-09-27 14:32:03 +0530

    Update Pascal's Triangle II.java

--- Pascal's Triangle II.java
+++ Pascal's Triangle II.java
@@ -31,4 +31,19 @@ class Solution {
     	else return pascal(row-1,col-1) + pascal(row-1,col);
     }
 }
-SOLUTION 3
+SOLUTION 3//-------------------------------------------------------
+class Solution {
+	public static List<Integer> getRow(int rowIndex) {
+		List<Integer> prev = new ArrayList<>();
+		prev.add(1);
+		return Pascal(prev, rowIndex);
+	}
+	public static List<Integer> Pascal(List<Integer> prev, int rowIndex){
+		if(rowIndex == prev.size()-1) return prev;
+		List<Integer> cur = new ArrayList<>();
+		cur.add(1);
+		for(int i=0;i<prev.size()-1;i++) cur.add(prev.get(i)+prev.get(i+1));
+		cur.add(1);
+		return Pascal(cur,rowIndex);
+	}
+}
