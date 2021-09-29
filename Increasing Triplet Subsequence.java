26e9eb108137c1471af62162536011fffc2f2afd
KetanGupta1818 <84444599+KetanGupta1818@users.noreply.github.com>
2021-09-29 10:17:31 +0530

    Create Increasing Triplet Subsequence.java

---
+++ Increasing Triplet Subsequence.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int small = Integer.MAX_VALUE;
+        int big = Integer.MAX_VALUE;
+        for(int n: nums){
+            if(n <=small) small = n;
+            else if(n <=big) big = n;
+            else return true;
+        }
+        return false;
+    }
+}
