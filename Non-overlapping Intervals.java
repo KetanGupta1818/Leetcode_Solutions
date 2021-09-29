25188f624bbbd06d6aa278855e9e61d01a83bb53
KetanGupta1818 <84444599+KetanGupta1818@users.noreply.github.com>
2021-09-29 09:33:55 +0530

    Update Non-overlapping Intervals.java

--- Non-overlapping Intervals.java
+++ Non-overlapping Intervals.java
@@ -25,3 +25,23 @@ class Solution {
         return result;
     }
 }
+/**************************************************************************************Method 2
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a,b)->Integer.compare(a[0],b[0]));
+        for(int i=0;i<intervals.length;i++)System.out.println(intervals[i][0]+" "+intervals[i][1]);
+        int ans=0;
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i-1][1]<=intervals[i][0]) intervals[i][0] = intervals[i-1][0];
+            else if(intervals[i-1][1]>=intervals[i][1] && intervals[i-1][1]>intervals[i][0]){
+                ans++;
+            }
+            else{
+                ans++;
+                intervals[i][0] = intervals[i-1][0];
+                intervals[i][1] = intervals[i-1][1];
+            } 
+        }
+        return ans;
+    }
+}
