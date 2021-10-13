ac86e9c0dd79616a56b9174138aeeaea23f3389a
KetanGupta1818 <84444599+KetanGupta1818@users.noreply.github.com>
2021-10-13 15:44:19 +0530

    Create Keys and Rooms.java

---
+++ Keys and Rooms.java
@@ -0,0 +1,18 @@
+class Solution {
+    boolean[] visited;
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        visited = new boolean[rooms.size()];
+        visited[0] = true;
+        dfs(rooms,0);
+        for(boolean b: visited) if(b==false) return false;
+        return true;
+    }
+    public void dfs(List<List<Integer>> rooms,int index){
+        for(int i: rooms.get(index)){
+            if(!visited[i]){
+                visited[i]=true;
+                dfs(rooms,i);
+            }
+        }
+    }
+}
