1a9e1989caece9a41df261ee50ff85ae25f0e6c0
KetanGupta1818 <84444599+KetanGupta1818@users.noreply.github.com>
2021-10-21 10:06:07 +0530

    Update Insert Delete GetRandom O(1).java

--- Insert Delete GetRandom O(1).java
+++ Insert Delete GetRandom O(1).java
@@ -21,3 +21,39 @@ class RandomizedSet {
         return list.get(random.nextInt(list.size()));
     }
 }
+//Method 2
+class RandomizedSet {
+    List<Integer> list;
+    HashMap<Integer, Integer> map;
+    public RandomizedSet() {
+        list = new LinkedList<>();
+        map = new HashMap<>();
+    }
+    
+    public boolean insert(int val) {
+        if(map.containsKey(val)) return false;
+        else{
+            list.add(val);
+            map.put(val,list.size()-1);
+        }
+        return true;
+    }
+    
+    public boolean remove(int val) {
+        if(!map.containsKey(val)) return false;
+        int index = map.get(val);
+        if(index<list.size()-1){
+            int last_value = list.get(list.size()-1);
+            list.set(map.get(val),last_value);
+            map.put(last_value,map.get(val));
+        }
+        map.remove(val);
+        list.remove(list.size()-1);
+        return true;
+    }
+    
+    public int getRandom() {
+        Random random = new Random();
+        return list.get(random.nextInt(list.size()));
+    }
+}
