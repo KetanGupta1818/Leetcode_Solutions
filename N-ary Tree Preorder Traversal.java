e4928501ccd42e434e83b69659efd09c702ef2fa
KetanGupta1818 <84444599+KetanGupta1818@users.noreply.github.com>
2021-11-12 09:55:37 +0530

    Update N-ary Tree Preorder Traversal.java

--- N-ary Tree Preorder Traversal.java
+++ N-ary Tree Preorder Traversal.java
@@ -9,3 +9,25 @@ class Solution {
         return preorderList;
     }
 }
+
+//Iterative solution
+class Solution{
+    public List<Integer> preorder(Node root){
+        List<Integer> preorderList = new ArrayList<>();
+        Stack<Node> stk = new Stack<>();
+        if(root == null) return preorderList;
+        stk.push(root);
+        while(!stk.isEmpty()){
+            Node temp = stk.pop();
+            preorderList.add(temp.val);
+            //System.out.println("Hello");
+            if(temp.children!=null) {
+                Collections.reverse(temp.children);
+                for(Node c : temp.children) stk.push(c);
+            }
+        }
+        return preorderList;
+    }
+}
+
+
