9ecfc600be9f3a462fa538ae2ecc186af0f3509a
KetanGupta1818 <84444599+KetanGupta1818@users.noreply.github.com>
2021-12-05 19:20:20 +0530

    Update Flood Fill.java

--- Flood Fill.java
+++ Flood Fill.java
@@ -18,3 +18,19 @@ class Solution {
         if(j<image[0].length-1 && image[i][j+1]==key) helper(image,i,j+1,newColor);
     }
 }
+//0 ms
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int key = image[sr][sc];
+        if(key==newColor) return image;
+        helper(image,sr,sc,key,newColor);
+        return image;
+    }
+    public void helper(int[][] image,int i, int j,int key, int newColor){
+        image[i][j] = newColor;
+        if(i>0 && image[i-1][j]==key) helper(image,i-1,j,key,newColor);
+        if(j>0 && image[i][j-1]==key) helper(image,i,j-1,key,newColor);
+        if(i<image.length-1 && image[i+1][j]==key) helper(image,i+1,j,key,newColor);
+        if(j<image[0].length-1 && image[i][j+1]==key) helper(image,i,j+1,key,newColor);
+    }
+}
