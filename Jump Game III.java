225c9dc786e80385a7836453aafc337cc7e833c0
KetanGupta1818 <84444599+KetanGupta1818@users.noreply.github.com>
2021-12-09 16:56:08 +0530

    Create Jump Game III.java

---
+++ Jump Game III.java
@@ -0,0 +1,18 @@
+class Solution {
+        public boolean canReach(int[] arr, int start) {
+        Queue<Integer> indices = new ArrayDeque<>();
+        boolean[] visitedIndex = new boolean[arr.length];
+        indices.offer(start);
+        while(!indices.isEmpty()){
+            int currentIndex = indices.poll();
+            if(visitedIndex[currentIndex]) continue;
+            visitedIndex[currentIndex] = true;
+            if(arr[currentIndex]==0) return true;
+            int aheadIndex = currentIndex + arr[currentIndex];
+            int behindIndex = currentIndex - arr[currentIndex];
+            if(aheadIndex<arr.length && !visitedIndex[aheadIndex]) indices.offer(aheadIndex);
+            if(behindIndex>=0 && !visitedIndex[behindIndex]) indices.offer(behindIndex);
+        }
+        return false;
+    }
+}
