363651d45c738a6c882f007e0cf6c3fa31cf493e
Ketan Gupta <84444599+KetanGupta1818@users.noreply.github.com>
2022-01-11 12:53:07 +0530

    Create Matrix Block Sum.java

---
+++ Matrix Block Sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[][] matrixBlockSum(int[][] mat, int K) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[][] sum = new int[m+1][n+1];
+        for(int r=1;r<=m;r++){
+            for(int c=1;c<=n;c++){
+                sum[r][c] = mat[r-1][c-1] + sum[r-1][c] + sum[r][c-1] - sum[r-1][c-1];
+            }
+        }
+        int[][] ans = new int[m][n];
+        for(int r=0;r<m;r++){
+            for(int c=0;c<n;c++){
+                int r1 = Math.max(0, r-K);
+                int c1 = Math.max(0, c-K);
+                int r2 = Math.min(m-1, r+K);
+                int c2 = Math.min(n-1, c+K);
+                r1++;r2++;c1++;c2++;
+                ans[r][c] = sum[r2][c2] - sum[r2][c1-1] - sum[r1-1][c2] + sum[r1-1][c1-1];
+            }
+        }
+        return ans;
+    }
+}
