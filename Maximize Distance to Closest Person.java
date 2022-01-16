0d7ad68fe069fa38c149de0f9de7240e9b0dfd87
Ketan Gupta <84444599+KetanGupta1818@users.noreply.github.com>
2022-01-16 11:11:16 +0530

    Create Maximize Distance to Closest Person.java

---
+++ Maximize Distance to Closest Person.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxDistToClosest(int[] seats) {
+        int n = seats.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Arrays.fill(left, n);
+        Arrays.fill(right, n);
+        for(int i=0;i<n;i++){
+            if(seats[i] == 1) left[i] = 0;
+            else if(i>0) left[i] = left[i-1]+1;
+        }
+        for(int i=n-1;i>=0;i--){
+            if(seats[i]==1) right[i]=0;
+            else if(i<n-1) right[i] = right[i+1] + 1;
+        }
+        int max = 1;
+        for(int i=0;i<n;i++){
+            max = Math.max(max,Math.min(left[i],right[i]));
+        }
+        return max;
+    }
+}
