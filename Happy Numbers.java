721f147d8f862888fa4373124a257c339cd9a303
Ketan Gupta <84444599+KetanGupta1818@users.noreply.github.com>
2022-03-03 13:30:06 +0530

    Create Happy Numbers.java

---
+++ Happy Numbers.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isHappy(int n) {
+        HashSet<Integer> set = new HashSet<>();
+        while(n!=1){
+            if(set.contains(n)) return false;
+            set.add(n);
+            int sum = 0;
+            while(n>0){
+                sum += (n%10)*(n%10);
+                n/=10;
+            }
+            n=sum;
+        }
+        return true;
+    }
+}
