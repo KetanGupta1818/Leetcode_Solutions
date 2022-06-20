217d5cf90883274eb001797e477dda2dc49fec35
Ketan Gupta <84444599+KetanGupta1818@users.noreply.github.com>
2022-06-20 13:15:47 +0530

    Create Short Encoding of Words.java

---
+++ Short Encoding of Words.java
@@ -0,0 +1,73 @@
+class Solution {
+    public int minimumLengthEncoding(String[] words) {
+        List<String> listOfWords = new ArrayList<>();
+        for(String w: words) listOfWords.add(new StringBuilder(w).reverse().toString());
+        Collections.sort(listOfWords,(a,b)->b.length()-a.length());
+        Trie trie = new Trie();
+        int answer = 0;
+        for(String word: listOfWords){
+            if(!trie.startsWith(word)) {
+                answer+=word.length()+1;
+                trie.insert(word);
+            }
+        }
+        return answer;
+    }
+}
+class Trie {
+    TrieNode root;
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for(int i=0;i<word.length();i++){
+            char curChar = word.charAt(i);
+            if(!node.hasLinkTo(curChar)) node.putLinkTo(curChar,new TrieNode());
+            node = node.getLinkTo(curChar);
+        }
+        node.setIsEnd();
+    }
+    public TrieNode searchPrefix(String word){
+        TrieNode node = root;
+        for(int i=0;i<word.length();i++){
+            char curChar = word.charAt(i);
+            if(!node.hasLinkTo(curChar)) return null;
+            node = node.getLinkTo(curChar);
+        }
+        return node;
+    }
+    public boolean search(String word) {
+        TrieNode node = searchPrefix(word);
+        return node!=null && node.getIsEnd();
+    }
+
+    public boolean startsWith(String prefix) {
+        return searchPrefix(prefix)!=null;
+    }
+}
+class TrieNode{
+    private TrieNode[] links;
+    private int R;
+    private boolean isEnd;
+    public TrieNode(){
+        this.R = 26;
+        this.links = new TrieNode[R];
+    }
+    public boolean hasLinkTo(char ch){
+        return this.links[ch-'a']!=null;
+    }
+    public TrieNode getLinkTo(char ch){
+        return this.links[ch-'a'];
+    }
+    public void putLinkTo(char ch, TrieNode node){
+        this.links[ch-'a']=node;
+    }
+    public void setIsEnd(){
+        this.isEnd = true;
+    }
+    public boolean getIsEnd(){
+        return this.isEnd;
+    }
+}
